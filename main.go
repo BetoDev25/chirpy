50c7ebeb57192f240f68ea107ae1127064880401
NorMeni <BetoDev25@users.noreply.github.com>
2026-01-06 17:34:35 -0500

    middleware function, metrics and reset functions

--- main.go
+++ main.go
@@ -3,17 +3,61 @@ package main
 import (
 	"net/http"
 	"log"
+	"sync/atomic"
+	"fmt"
 )
+
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
+
+func handlerHealthz(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("OK"))
+}
+
+func (cfg *apiConfig) handlerReset(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	cfg.fileserverHits.Store(0)
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("OK"))
+}
+
+func (cfg *apiConfig) handlerMetrics(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	hits := cfg.fileserverHits.Load()
+	body := fmt.Sprintf(`<html>
+			<body>
+				<h1>Welcome, Chirpy Admin</h1>
+				<p>Chirpy has been visited %d times!</p>
+			</body>
+		</html>`, hits)
+	w.Write([]byte(body))
+}
 
 func main() {
 	serve := http.NewServeMux()
+	apiCfg := apiConfig{}
 
 	server := &http.Server{
 		Addr:    ":8080",
 		Handler: serve,
 	}
 
-	serve.Handle("/", http.FileServer(http.Dir(".")))
+	serve.Handle("/app/", apiCfg.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir(".")))))
+
+	serve.HandleFunc("GET /api/healthz", handlerHealthz)
+	serve.HandleFunc("GET /admin/metrics", apiCfg.handlerMetrics)
+	serve.HandleFunc("POST /admin/reset", apiCfg.handlerReset)
 
 	err := server.ListenAndServe()
 	if err != nil {
