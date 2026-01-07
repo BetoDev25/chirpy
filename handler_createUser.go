6af603cf81a2c592354b3bde592013b38ed30530
NorMeni <BetoDev25@users.noreply.github.com>
2026-01-07 15:09:13 -0500

    added Postgresql functionality

---
+++ handler_createUser.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"encoding/json"
+	"net/http"
+	"time"
+	"fmt"
+
+	"github.com/google/uuid"
+)
+
+type User struct {
+	ID        uuid.UUID `json:"id"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+	Email     string    `json:"email"`
+}
+
+func (cfg *apiConfig) handlerCreateUser(w http.ResponseWriter, r *http.Request) {
+	type params struct {
+		Email string `json:"email"`
+	}
+
+	decoder := json.NewDecoder(r.Body)
+	input := params{}
+	err := decoder.Decode(&input)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Couldn't decode input")
+		return
+	}
+
+	user, err := cfg.database.CreateUser(r.Context(), input.Email)
+	if err != nil {
+		fmt.Println("CreateUser error:", err)
+		respondWithError(w, http.StatusInternalServerError, "Couldn't create user")
+		return
+	}
+
+	respondWithJSON(w, http.StatusCreated, User{
+		ID: 	   user.ID,
+		CreatedAt: user.CreatedAt,
+		UpdatedAt: user.UpdatedAt,
+		Email:     user.Email,
+	})
+}
--- main.go
+++ main.go
@@ -5,10 +5,48 @@ import (
 	"log"
 	"sync/atomic"
 	"fmt"
+	"encoding/json"
+	"strings"
+	"database/sql"
+	"os"
+
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
+
+	"github.com/BetoDev25/chirpy/internal/database"
 )
+
+func respondWithError(w http.ResponseWriter, code int, msg string) {
+	type errorMsg struct {
+		Error string `json:"error"`
+	}
+
+	resp := errorMsg{
+		Error: msg,
+	}
+	w.Header().Set("Content-Type", "application/json")
+	dat, err := json.Marshal(resp)
+	if err != nil {
+		w.WriteHeader(500)
+		return
+	} else {
+		w.WriteHeader(code)
+	}
+
+	w.Write(dat)
+}
+
+func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(code)
+
+	json.NewEncoder(w).Encode(payload)
+}
 
 type apiConfig struct {
 	fileserverHits atomic.Int32
+	database       *database.Queries
+	platform       string
 }
 
 func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
@@ -25,10 +63,18 @@ func handlerHealthz(w http.ResponseWriter, r *http.Request) {
 }
 
 func (cfg *apiConfig) handlerReset(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	if cfg.platform != "dev" {
+		respondWithError(w, http.StatusForbidden, "Platform is not dev")
+		return
+	}
+	err := cfg.database.Reset(r.Context())
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Couldn't reset")
+		return
+	}
 	cfg.fileserverHits.Store(0)
-	w.WriteHeader(http.StatusOK)
-	w.Write([]byte("OK"))
+
+	respondWithJSON(w, http.StatusOK, struct{}{})
 }
 
 func (cfg *apiConfig) handlerMetrics(w http.ResponseWriter, r *http.Request) {
@@ -44,9 +90,45 @@ func (cfg *apiConfig) handlerMetrics(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte(body))
 }
 
+func (cfg *apiConfig) handlerChirp(w http.ResponseWriter, r *http.Request) {
+	type parameters struct {
+		Body string `json:"body"`
+	}
+	decoder := json.NewDecoder(r.Body)
+	params := parameters{}
+	err := decoder.Decode(&params)
+	if err != nil {
+		respondWithError(w, 500, "Something went wrong")
+		return
+	} else if len(params.Body) > 140 {
+		respondWithError(w, 400, "Chirp is too long")
+		return
+	}
+
+	words := strings.Split(params.Body, " ")
+	for i, word := range words {
+		lower := strings.ToLower(word)
+		if lower == "kerfuffle" || lower == "sharbert" || lower == "fornax" {
+			words[i] = "****"
+		}
+	}
+	cleanSentence := strings.Join(words, " ")
+	respondWithJSON(w, 200, map[string]string{"cleaned_body": cleanSentence})
+}
+
 func main() {
+	godotenv.Load()
+	dbURL := os.Getenv("DB_URL")
+	plat := os.Getenv("PLATFORM")
+	db, err := sql.Open("postgres", dbURL)
+	dbQueries := database.New(db)
+
 	serve := http.NewServeMux()
-	apiCfg := apiConfig{}
+	apiCfg := apiConfig{
+		fileserverHits: atomic.Int32{},
+		database:       dbQueries,
+		platform:       plat,
+	}
 
 	server := &http.Server{
 		Addr:    ":8080",
@@ -58,8 +140,10 @@ func main() {
 	serve.HandleFunc("GET /api/healthz", handlerHealthz)
 	serve.HandleFunc("GET /admin/metrics", apiCfg.handlerMetrics)
 	serve.HandleFunc("POST /admin/reset", apiCfg.handlerReset)
+	serve.HandleFunc("POST /api/validate_chirp", apiCfg.handlerChirp)
+	serve.HandleFunc("POST /api/users", apiCfg.handlerCreateUser)
 
-	err := server.ListenAndServe()
+	err = server.ListenAndServe()
 	if err != nil {
 		log.Fatal(err)
 	}
